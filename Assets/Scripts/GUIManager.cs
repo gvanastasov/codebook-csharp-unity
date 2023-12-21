c8142a8e8657feedccf7cb66ca97be9d76dfc622
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2023-12-21 16:13:20 +0100

    Add dropdowns for chapter and part selection

--- Assets/Scripts/GUIManager.cs
+++ Assets/Scripts/GUIManager.cs
@@ -4,11 +4,25 @@ using UnityEngine;
 
 public class GUIManager : MonoBehaviour
 {
+#region Static Members
     public static GUIManager Instance;
+#endregion
 
+#region Serialized Fields
     public TMP_Dropdown ChapterDropdown;
 
     public TMP_Dropdown PartDropdown;
+#endregion
+
+#region Public Properties
+    public string SelectedChapterName
+    {
+        get
+        {
+            return ChapterDropdown.options[ChapterDropdown.value].text;
+        }
+    }
+#endregion
 
     private void Awake()
     {
@@ -37,7 +51,7 @@ public class GUIManager : MonoBehaviour
         if (options.Count == 0)
         {
             PartDropdown.interactable = false;
-            PartDropdown.AddOptions(new List<string> { "n/a" });
+            Debug.LogWarning("No parts found for this chapter.");
         }
         else
         {
