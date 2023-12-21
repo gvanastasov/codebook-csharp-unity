59af9231183f93f5e3b1205d664e5dcb564cca41
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2023-12-21 02:19:07 +0100

    chapter 2 part 1

---
+++ Assets/Scripts/Chapter_02/LearnVariables.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class LearnVariables : MonoBehaviour
+{
+    public int CurrentAge = 30;
+    void Start()
+    {
+        Debug.Log(30 + 1);
+        Debug.Log(CurrentAge + 1);
+    }
+}
