2277d8928eb288a288acfbad6287d672f0da5cdb
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2023-12-22 00:03:30 +0100

    Refactor Vars.cs: Add public variables and remove unused code

--- Assets/Scripts/Chapter_02/Vars.cs
+++ Assets/Scripts/Chapter_02/Vars.cs
@@ -1,21 +1,26 @@
 using UnityEngine;
 
+// Lets ignore this for now.
 public class Vars : MonoBehaviour
 {
     // This is a public variable.
+    // This is a class scope variable.
     // This is serialized and can be set in the inspector.
     public int UniverseSecret = 42;
 
-    // This is a private variable.
-    // This is not serialized and cannot be set in the inspector.
-    // private int UniverseSecret_Private = 42;
+    [Header("Other Common Variables")]
+    public float Speed = 1.0f;
+    public string Name = "John Doe";
+    public bool IsAlive = true;
 
-    // This is a static variable.
-    // This is not serialized and cannot be set in the inspector.
-    // public static int UniverseSecret_Static = 42;
-
-    void Start()
+    // Lets ignore this for now.
+    private void Start()
     {
         Debug.Log("c02p01 :: Vars :: double click me to see the code! PS: " + UniverseSecret + " is the answer to life, the universe and everything.");
+
+        // This is a local variable.
+        // This is a method scope variable.
+        // This is not serialized and can not be set in the inspector.
+        int localSecret = 42;
     }
 }
