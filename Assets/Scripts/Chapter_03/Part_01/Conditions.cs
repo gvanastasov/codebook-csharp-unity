73777b96b238c84dcc1d084adeb11c9d7efcf03e
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2023-12-22 22:21:56 +0100

    Add switch case example

--- Assets/Scripts/Chapter_03/Part_01/Conditions.cs
+++ Assets/Scripts/Chapter_03/Part_01/Conditions.cs
@@ -12,11 +12,11 @@ public class Conditions : MonoBehaviour
         // If the condition is false, the code inside the block is skipped.
         if (this.transform.position.x < 0)
         {
-            Debug.Log("c03p01 :: Conditions :: OnMouseDown() :: x < 0 and the cube is on the LEFT side of the screen.");
+            Debug.Log("c03p01 :: Conditions :: OnMouseDown() :: x < 0 and the cube is on the LEFT side of the screen. Click me to see more.");
         }
         else
         {
-            Debug.Log("c03p01 :: Conditions :: OnMouseDown() :: x >= 0 and the cube is on the RIGHT side of the screen.");
+            Debug.Log("c03p01 :: Conditions :: OnMouseDown() :: x >= 0 and the cube is on the RIGHT side of the screen. Click me to see more.");
         }
     }
 
@@ -48,6 +48,41 @@ public class Conditions : MonoBehaviour
             {
                 return "This is a digit between 1 and 4!";
             }
+        }
+    }
+
+    private string GetWeekdayNameByNumber(int number)
+    {
+        // Either the first condition is true, or the second one, or the third one, etc.
+        // OR = ||
+        if (number < 0 || number > 7)
+        {
+            Debug.LogError("c03p01 :: Conditions :: GetWeekdayNameByNumber() :: not a valid weekday number!");
+        }
+
+        // Switch is a special type of condition.
+        // It checks if the value of the variable is equal to the value in the case.
+        switch (number)
+        {
+            // If it is, it executes the code in the case.
+            // If it is not, it skips the code in the case, and checks the next case.
+            case 1:
+                return "Monday";
+            case 2:
+                return "Tuesday";
+            case 3:
+                return "Wednesday";
+            case 4:
+                return "Thursday";
+            case 5:
+                return "Friday";
+            case 6:
+                return "Saturday";
+            case 7:
+                return "Sunday";
+            // If no case is true, it executes the code in the default case.
+            default:
+                return "Invalid weekday number!";
         }
     }
 }
