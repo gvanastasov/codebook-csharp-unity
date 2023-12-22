85179b07445f050f0c07e7bc93e1c509f4dc4a0f
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2023-12-22 22:08:32 +0100

    Refactor OnMouseDown method and add MagicWithDigits method

--- Assets/Scripts/Chapter_03/Part_01/Conditions.cs
+++ Assets/Scripts/Chapter_03/Part_01/Conditions.cs
@@ -5,7 +5,7 @@ public class Conditions : MonoBehaviour
     // Unity reserved method name.
     // This method is called once you click on an object in Unity that has 
     // a collider (will talk about that later).
-    void OnMouseDown()
+    private void OnMouseDown()
     {
         // This is a CONDITION. It is a block of code that CHECKS something.
         // If the condition is true, the code inside the block is executed.
@@ -17,6 +17,36 @@ public class Conditions : MonoBehaviour
         else
         {
             Debug.Log("c03p01 :: Conditions :: OnMouseDown() :: x >= 0 and the cube is on the RIGHT side of the screen.");
+        }
+    }
+
+    private string MagicWithDigits(int number)
+    {
+        // We can also have multiple blocks of code, each with its own condition.
+        if (number > 9)
+        {
+            return "This is not a digit!";
+        }
+        else if (number < 0)
+        {
+            return "This also is not a digit!";
+        }
+        else
+        {
+            // We can also have nested conditions.
+            // We can also have multiple conditions in one block.
+            if (number > 4 && number < 9)
+            {
+                return "This is a digit between 5 and 8!";
+            }
+            else if (number == 0)
+            {
+                return "This is a magic digit!";
+            }
+            else
+            {
+                return "This is a digit between 1 and 4!";
+            }
         }
     }
 }
