eefd96d93a126763abaaa7e54d16adeeb8d5b91e
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2023-12-27 16:15:04 +0100

    Add TextMeshProUGUI reference and set text

--- Assets/Scripts/Chapter_02/Part_01/Vars.cs
+++ Assets/Scripts/Chapter_02/Part_01/Vars.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 // Lets ignore this for now.
@@ -13,6 +14,8 @@ public class Vars : MonoBehaviour
     public string Name = "John Doe";
     public bool IsAlive = true;
 
+    public TextMesh Text;
+
     // Lets ignore this for now.
     private void Start()
     {
@@ -24,5 +27,11 @@ public class Vars : MonoBehaviour
         // Because we don't really use this anywhere, but alocated space for it, both your IDE and 
         // Unity will warn you about it - don't waste your computer's memory!
         int localSecret = 42;
+
+        // Ignore this for now - otherwise its a reference to a TextMeshProUGUI component, to which we set the text.
+        if (Text != null)
+        {
+            Text.text = this.UniverseSecret.ToString();
+        }
     }
 }
